3d929360441c5b957a624d41183f05ee5d4624ff
Brandon K. Waltz <bwaltz6@gmail.com>
2025-07-28 07:28:15 -0400

    dev to master for DAI-colby release

--- scripts/dependencies-run-install-build.sh
+++ scripts/dependencies-run-install-build.sh
@@ -7,5 +7,22 @@ shopt -s extglob # Turns on extended globbing
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
+printf "Build Colby Theme... \n"
+cd web/wp-content/themes/colby-college-theme
+composer install
+composer dump-autoload
+yarn
+yarn scripts:build
+cd -
+
+printf "Build Davis AI Theme... \n"
+cd web/wp-content/themes/colby-davis-ai-theme
+composer install
+composer dump-autoload
+yarn
+yarn scripts:build
+cd -
+
+
 # npm install
 shopt -u extglob
