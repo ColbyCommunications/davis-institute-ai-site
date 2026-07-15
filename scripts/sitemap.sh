234bd76268d0c0a497b04dfcdf764712b9fb3eb3
Brandon K. Waltz <bwaltz6@gmail.com>
2026-07-15 14:47:31 -0400

    Update sitemap.sh

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -1,4 +1,4 @@
 EDGE_HOSTNAME=$(upsun environment:info edge_hostname)
-JQ="https://${EDGE_HOSTNAME}"
+JQ="https://davisinstituteai.colby.edu"
 echo $(cat .github/sitemap.json | jq -r --arg JQ "$JQ" '.urls += [$JQ]') > .github/sitemap.json
 flatten_sitemap --sitemap https://$EDGE_HOSTNAME/sitemap_index.xml --config .github/sitemap.json --limit 25 --randomize
