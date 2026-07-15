08fb298a88a1d28435c1bf2e3e8ef6e5286c34fc
Brandon K. Waltz <bwaltz6@gmail.com>
2026-07-15 14:30:35 -0400

    Update sitemap_master.sh

--- scripts/sitemap_master.sh
+++ scripts/sitemap_master.sh
@@ -1,4 +1,4 @@
 EDGE_HOSTNAME=$(upsun domains --format=plain --no-header --columns=name )
-JQ="https://${EDGE_HOSTNAME}"
+JQ="https://davisinstituteai.colby.edu"
 echo $(cat .github/sitemap.json | jq -r --arg JQ "$JQ" '.urls += [$JQ]') > .github/sitemap.json
 cat .github/sitemap.json
